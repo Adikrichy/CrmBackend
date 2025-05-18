f52ae19cb014ddd00aee820ae58b75fed32058a5
Adikrichy <adil.erzhanoc.70@gmail.com>
2025-05-18 17:30:10 +0500

    staff-service

---
+++ staff-service/src/main/java/org/aldousdev/staffservice/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package org.aldousdev.staffservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain springSecurityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/v1/staff/**").hasAnyRole("CEO","DIRECTOR","MANAGER")
+                        .requestMatchers("/api/v1/tasks/**").hasAnyRole("CEO","DIRECTOR","MANAGER","WORKER")
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .build();
+
+
+    }
+}
