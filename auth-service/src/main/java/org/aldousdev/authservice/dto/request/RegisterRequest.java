a8adf20a723fe88e35bff349423f8b76c7e57dc5
Adikrichy <adil.erzhanoc.70@gmail.com>
2025-05-20 02:15:31 +0500

    Init the 2 dto Login and register

---
+++ auth-service/src/main/java/org/aldousdev/authservice/dto/request/RegisterRequest.java
@@ -0,0 +1,21 @@
+package org.aldousdev.authservice.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import java.util.Set;
+
+@Data
+public class RegisterRequest {
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    private String password;
+
+    @Email
+    @NotBlank
+    private String email;
+
+    private Set<String> roles;
+}
