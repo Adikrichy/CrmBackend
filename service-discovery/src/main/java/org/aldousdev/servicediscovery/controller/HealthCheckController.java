b74405acf91a1c98352765574dc70418656e973e
Adikrichy <adil.erzhanoc.70@gmail.com>
2025-05-19 16:59:53 +0500

    End the Eureka server, but im not add the client for other services

---
+++ service-discovery/src/main/java/org/aldousdev/servicediscovery/controller/HealthCheckController.java
@@ -0,0 +1,17 @@
+// HealthCheckController.java
+package org.aldousdev.servicediscovery.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/actuator")
+public class HealthCheckController {
+
+    @GetMapping("/health")
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok("Eureka Server is running");
+    }
+}
