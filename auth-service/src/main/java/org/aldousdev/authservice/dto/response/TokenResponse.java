3c80fa5f17bb926fe440e86fbf5d509610febcd4
Adikrichy <adil.erzhanoc.70@gmail.com>
2025-05-20 02:17:29 +0500

    Add the TokenResponse

---
+++ auth-service/src/main/java/org/aldousdev/authservice/dto/response/TokenResponse.java
@@ -0,0 +1,17 @@
+package org.aldousdev.authservice.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenResponse {
+    private String accessToken;
+    private String refreshToken;
+    private String tokenType;
+    private Long expiresIn;
+}
