7d94afc0c898401f582b8ccda8685334bd82fc3f
Adikrichy <adil.erzhanoc.70@gmail.com>
2025-05-19 16:51:07 +0500

    Change the Application RabbitMQ

--- staff-service/src/main/java/org/aldousdev/staffservice/config/RabbitMQConfig.java
+++ staff-service/src/main/java/org/aldousdev/staffservice/config/RabbitMQConfig.java
@@ -6,9 +6,8 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.naming.Binding;
-import java.util.Queue;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.Queue;
 
 @Configuration
 public class RabbitMQConfig {
