f04c6921dea91252c8dcb3de183386728d063299
Adikrichy <adil.erzhanoc.70@gmail.com>
2025-05-23 00:56:19 +0500

    Make the Sale Entity

--- analytics-service/src/main/java/org/aldousdev/analyticsservice/AnalyticsServiceApplication.java
+++ analytics-service/src/main/java/org/aldousdev/analyticsservice/AnalyticsServiceApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AnalyticsServiceApplication {
+public class    AnalyticsServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AnalyticsServiceApplication.class, args);
---
+++ analytics-service/src/main/java/org/aldousdev/analyticsservice/entity/Sale.java
@@ -0,0 +1,31 @@
+package org.aldousdev.analyticsservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Sale {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private UUID userId;
+    private UUID productId;
+    private int quantity;
+    private BigDecimal salesPrice;
+    private BigDecimal costPrice;
+    private double totalRevenue;
+    private double totalCost;
+    private double profit;
+    private LocalDateTime saleDate;
+    private UUID orderId;
+}
